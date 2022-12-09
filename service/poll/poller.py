c4824c4564b7143dc8bf3a72014306e9a2bd8147
tracey chung <tchung97@gmail.com>
2022-12-09 09:20:50 -0800

    Minor updates

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -12,17 +12,6 @@ django.setup()
 # Import models from service_rest, here.
 from service_rest.models import AutomobileVO
 
-# def get_vins():
-#     response = requests.get("http://inventory-api:8000/api/automobiles/")
-#     content = json.loads(response.content)
-#     for auto in content["autos"]:
-#         AutomobileVO.objects.update_or_create(
-#             import_href=auto["href"],
-#             defaults={
-#                 "vin": auto["vin"],
-#             }
-#         )
-
 def get_automobiles():
     url = "http://inventory-api:8000/api/automobiles/"
     response = requests.get(url)
